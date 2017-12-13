a7eaf917d71dfe67d6e6d483e6ebe212b3c53591
贾昌鑫 <jiachx@thunisoft.com>
2017-12-13 17:25:39 +0800

    fix bug: unknow warpctc layer

--- .build_release/src/caffe/bktree.d
+++
@@ -1,3 +0,0 @@
-.build_release/src/caffe/bktree.o: src/caffe/bktree.cpp include/bktree.h
-
-include/bktree.h:
--- .build_release/src/caffe/util/db_leveldb.d
+++
@@ -1,1 +0,0 @@
-.build_release/src/caffe/util/db_leveldb.o: src/caffe/util/db_leveldb.cpp
